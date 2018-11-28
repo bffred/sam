4e30519840b6cf3ae4161ebf94bbcb80c0e8e4f7
unknown <bffred@orange.fr>
2018-11-28 09:40:43 +0100

    mise a jours des pages sans les détails

--- Faience.php
+++ Faience.php
@@ -169,27 +169,11 @@
 											<option>6</option>
 										</select>
 									</li>
-									<li class="shop_btn"><br><button href="single.html">Détails</button></li>
+									
 									<li class="cart"><br><button href="#">Ajouter au panier</button></li>
 									<div class="clear"> </div>
 								</ul><br>
-								<div class="desc">
-									<h4>Description</h4>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-									sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-									aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud 
-									exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex 
-									ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit 
-									in vulputate velit esse molestie consequat, vel illum dolore eu 
-									feugiat nulla facilisis at vero eros et accumsan et iusto odio 
-									dignissim qui blandit praesent luptatum zzril delenit augue duis 
-									dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis 
-									eleifend option congue nihil imperdiet doming id quod mazim placerat 
-									facer possim assum. Typi non habent claritatem insitam; est usus 
-									legentis in iis qui facit eorum claritatem. Investigationes 
-									legentis in iis qui facit eorum claritatem. Investigationes
-									demonstraverunt lectores</p>
-								</div>
+								
 							</div>
 						   </div>
 						</a>
--- Mosaique.php
+++ Mosaique.php
@@ -121,6 +121,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	  </div>
 	  <div class="border">
 	  <?php
+	  
         // $host ="samcarrecmsam.mysql.db";
         // $uname = "samcarrecmsam";
         // $pwd = '4Carrelage';
@@ -174,23 +175,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 									<li class="cart"><br><button href="#">Ajouter au panier</button></li>
 									<div class="clear"> </div>
 								</ul><br>
-								<div class="desc">
-									<h4>Description</h4>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-									sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-									aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud 
-									exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex 
-									ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit 
-									in vulputate velit esse molestie consequat, vel illum dolore eu 
-									feugiat nulla facilisis at vero eros et accumsan et iusto odio 
-									dignissim qui blandit praesent luptatum zzril delenit augue duis 
-									dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis 
-									eleifend option congue nihil imperdiet doming id quod mazim placerat 
-									facer possim assum. Typi non habent claritatem insitam; est usus 
-									legentis in iis qui facit eorum claritatem. Investigationes 
-									legentis in iis qui facit eorum claritatem. Investigationes
-									demonstraverunt lectores</p>
-								</div>
+								
 							</div>
 						   </div>
 						</a>
